fb161dfb66602fc27f4d8f2b1dd1fbe4806b8d2a
rafaelprogrammer <rafa.jesuscristo@gmail.com>
2022-10-14 23:45:44 -0300

    ajustando os perfis, deixando somente o de professor

--- src/main/java/br/com/kronos/backend/adaptadores/seguranca/Authority.java
+++ src/main/java/br/com/kronos/backend/adaptadores/seguranca/Authority.java
@@ -15,26 +15,7 @@ import br.com.kronos.backend.aplicacao.api.dto.PerfilDTO;
 public enum Authority implements GrantedAuthority, Serializable {
 
 	ADMINISTRADOR("Administrador", 2),
-	ADMINISTRADOR_DE_USUARIOS("Administrador de usuários", 3, "usuarios"),
-	MANTENEDOR("Mantenedor", 4),
-	DIRETOR("Diretor", 5, "tipos_ocorrencias", "ocorrencias", "relatorios_pedagogicos", "relatorios_alunos"),
-	VICE_DIRETOR("Vice Diretor", 6, "relatorios_pedagogicos", "relatorios_alunos"),
-	TESOUREIRO("Tesoureiro", 7, "contratos", "relatorios_pedagogicos", "relatorios_alunos"),
-	AUXILIAR_DE_TESOUREIRO("Auxiliar de Tesoureiro", 8, "contratos", "relatorios_pedagogicos", "relatorios_alunos"),
-	SECRETARIO("Secretário", 9, "pessoas", "cursos", "disciplinas",  "matriculas", "calendarios", "estruturas_anos_letivos", "tipos_ocorrencias", "ocorrencias", "atestados", "relatorios_pedagogicos", "relatorios_alunos"),
-	AUXILIAR_DE_SECRETARIO("Auxiliar de Secretário", 10, "pessoas", "cursos", "disciplinas",  "matriculas", "atestados", "relatorios_pedagogicos", "relatorios_alunos"),
-	RECURSOS_HUMANOS("Recursos Humanos", 11, "funcionarios", "homologacao_pontos"),
-	AUXILIAR_DE_RECURSOS_HUMANOS("Auxiliar de Recursos Humanos", 12, "funcionarios", "homologacao_pontos"),
-	COORDENADOR_DE_CURSO("Coordenador de curso", 13, "horarios", "estruturas_pedagogicas", "planos_pedagogicos", "tipos_ocorrencias", "ocorrencias", "relatorios_pedagogicos", "relatorios_alunos"),
-	COORDENADOR_PEDAGOGICO("Coordenador pedagógico", 14, "tipos_ocorrencias", "relatorios_pedagogicos", "relatorios_alunos"),
-	PROFESSOR("Professor", 15, "diarios", "frequencias", "avaliacoes", "resultados_avaliacoes_habilidades", "ocorrencias", "relatorios_pedagogicos", "relatorios_alunos", "resultados_bimestres"),
-	RESPONSAVEL_FINANCEIRO("Responsável Financeiro", 16),
-	RESPONSAVEL_PEDAGOGICO("Responsável Pedagógico", 17),
-	ALUNO("Aluno", 18),
-	BIBLIOTECARIO("Bibliotecário", 19),
-	AUXILIAR_DE_BIBLIOTECARIO("Auxiliar de Bibliotecário", 20),
-	ORIENTADOR_EDUCACIONAL("Orientador Educacional", 21, "tipos_ocorrencias", "ocorrencias"),
-	FUNCIONARIO("Funcionário", 22, "pontos");
+	PROFESSOR("Professor", 15, "diarios", "frequencias", "avaliacoes", "resultados_avaliacoes_habilidades", "ocorrencias", "relatorios_pedagogicos", "relatorios_alunos", "resultados_bimestres");
 	
 	private int id;
 	private String nome;
