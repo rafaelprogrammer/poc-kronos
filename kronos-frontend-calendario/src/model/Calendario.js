408cdb9ca8416e1cce5fb2175e19f3bb7885229a
rafaelprogrammer <rafa.jesuscristo@gmail.com>
2022-10-15 00:21:22 -0300

    removendo documentações

--- kronos-frontend-calendario/src/model/Calendario.js
+++
@@ -1,8 +0,0 @@
-class Calendario {
-  constructor(dados) {
-    this.id = dados ? dados.id : null;
-    this.dataInicio = dados ? dados.dataInicio : null;
-    this.dataFinal = dados ? dados.dataFinal : null;
-  }
-}
-export default Calendario;
--- kronos-frontend-calendario/src/model/ResultadoPesquisa.js
+++
@@ -1,7 +0,0 @@
-class ResultadoPesquisa {
-  constructor(dadosPaginacao) {
-    this.total = dadosPaginacao ? dadosPaginacao.total : null;
-    this.dados = dadosPaginacao ? dadosPaginacao.dados : null;
-  }
-}
-export default ResultadoPesquisa;
